ea8e0c8a3a4a0378bcb85179afc7d11533d7b2e6
맹순영 <symaeng98@naver.com>
2023-09-09 16:54:29 +0900

    chore: 프로젝트 설정

---
+++ src/main/java/com/musicpedia/musicpediaapi/MusicpediaApiApplication.java
@@ -0,0 +1,13 @@
+package com.musicpedia.musicpediaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicpediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicpediaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicpedia/musicpediaapi/MusicpediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicpedia.musicpediaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicpediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
