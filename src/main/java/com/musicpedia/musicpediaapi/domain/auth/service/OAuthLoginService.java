0b3a58fe80aadbb01da937c826c3b38dab36074e
맹순영 <symaeng98@naver.com>
2023-09-09 23:05:25 +0900

    feat: auth service 구현

---
+++ src/main/java/com/musicpedia/musicpediaapi/domain/auth/service/OAuthLoginService.java
@@ -0,0 +1,40 @@
+package com.musicpedia.musicpediaapi.domain.auth.service;
+
+import com.musicpedia.musicpediaapi.domain.auth.dto.OAuthInfoResponse;
+import com.musicpedia.musicpediaapi.domain.auth.dto.OAuthLoginParams;
+import com.musicpedia.musicpediaapi.domain.member.entity.Member;
+import com.musicpedia.musicpediaapi.domain.member.repository.MemberRepository;
+import com.musicpedia.musicpediaapi.global.util.AuthTokens;
+import com.musicpedia.musicpediaapi.global.util.AuthTokensGenerator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OAuthLoginService {
+    private final MemberRepository memberRepository;
+    private final AuthTokensGenerator authTokensGenerator;
+    private final RequestOAuthInfoService requestOAuthInfoService;
+
+    public AuthTokens login(OAuthLoginParams params) {
+        OAuthInfoResponse oAuthInfoResponse = requestOAuthInfoService.request(params);
+        Long memberId = findOrCreateMember(oAuthInfoResponse);
+        return authTokensGenerator.generate(memberId);
+    }
+
+    private Long findOrCreateMember(OAuthInfoResponse oAuthInfoResponse) {
+        return memberRepository.findByEmail(oAuthInfoResponse.getEmail())
+                .map(Member::getId)
+                .orElseGet(() -> newMember(oAuthInfoResponse));
+    }
+
+    private Long newMember(OAuthInfoResponse oAuthInfoResponse) {
+        Member member = Member.builder()
+                .email(oAuthInfoResponse.getEmail())
+                .nickname(oAuthInfoResponse.getNickname())
+                .oAuthProvider(oAuthInfoResponse.getOAuthProvider())
+                .build();
+
+        return memberRepository.save(member).getId();
+    }
+}
