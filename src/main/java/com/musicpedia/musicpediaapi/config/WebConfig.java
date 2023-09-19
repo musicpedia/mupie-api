db650028304672defc168621edd2843731296a3c
맹순영 <symaeng98@naver.com>
2023-09-20 01:21:19 +0900

    feat: Interceptor 구현

---
+++ src/main/java/com/musicpedia/musicpediaapi/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.musicpedia.musicpediaapi.config;
+
+import com.musicpedia.musicpediaapi.global.util.JwtUtil;
+import com.musicpedia.musicpediaapi.interceptor.JwtInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080") // 허용할 출처
+                .allowedMethods("GET","POST","PUT","DELETE") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new JwtInterceptor(jwtUtil))
+                .addPathPatterns("/v1/**")
+                .excludePathPatterns("/v1/auth/**");
+    }
+}
