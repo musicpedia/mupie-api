7212f2b0fb3b3c51ce003c1426832daf67b205e9
맹순영 <symaeng98@naver.com>
2023-09-20 15:22:06 +0900

    fix: json property 설정

--- src/main/java/com/musicpedia/musicpediaapi/domain/auth/dto/OAuthLoginParams.java
+++ src/main/java/com/musicpedia/musicpediaapi/domain/auth/dto/OAuthLoginParams.java
@@ -1,8 +1,10 @@
 package com.musicpedia.musicpediaapi.domain.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class OAuthLoginParams {
+    @JsonProperty("id_token")
     private String idToken;
 }
