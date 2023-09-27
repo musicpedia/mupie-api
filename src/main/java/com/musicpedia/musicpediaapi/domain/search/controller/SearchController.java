233012af8625f544514d70e6fb5e4172a4a9ef9a
맹순영 <symaeng98@naver.com>
2023-09-27 21:33:35 +0900

    feat: api 명세를 위한 틀 구현

--- src/main/java/com/musicpedia/musicpediaapi/domain/search/controller/SearchController.java
+++ src/main/java/com/musicpedia/musicpediaapi/domain/search/controller/SearchController.java
@@ -1,6 +1,9 @@
 package com.musicpedia.musicpediaapi.domain.search.controller;
 
 import com.musicpedia.musicpediaapi.domain.search.dto.SpotifySearchAlbumTrackArtistInfo;
+import com.musicpedia.musicpediaapi.domain.search.dto.SpotifySearchAlbumsInfo;
+import com.musicpedia.musicpediaapi.domain.search.dto.SpotifySearchArtistsInfo;
+import com.musicpedia.musicpediaapi.domain.search.dto.SpotifySearchTracksInfo;
 import com.musicpedia.musicpediaapi.domain.search.service.SearchService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -26,5 +29,38 @@ public class SearchController {
         long memberId = Long.parseLong(httpServletRequest.getAttribute("memberId").toString());
         SpotifySearchAlbumTrackArtistInfo searchInfo = searchService.getAllSearchInfo(memberId, keyword, offset, limit);
         return ResponseEntity.ok(searchInfo);
+    }
+
+    @GetMapping("/albums")
+    public ResponseEntity<SpotifySearchAlbumsInfo> searchAlbums(
+            @RequestParam(name = "keyword") String keyword,
+            @RequestParam(name = "offset") long offset,
+            @RequestParam(name = "limit") int limit,
+            HttpServletRequest httpServletRequest
+    ) {
+        long memberId = Long.parseLong(httpServletRequest.getAttribute("memberId").toString());
+        return ResponseEntity.ok(new SpotifySearchAlbumsInfo());
+    }
+
+    @GetMapping("/artists")
+    public ResponseEntity<SpotifySearchArtistsInfo> searchArtists(
+            @RequestParam(name = "keyword") String keyword,
+            @RequestParam(name = "offset") long offset,
+            @RequestParam(name = "limit") int limit,
+            HttpServletRequest httpServletRequest
+    ) {
+        long memberId = Long.parseLong(httpServletRequest.getAttribute("memberId").toString());
+        return ResponseEntity.ok(new SpotifySearchArtistsInfo());
+    }
+
+    @GetMapping("/tracks")
+    public ResponseEntity<SpotifySearchTracksInfo> searchTracks(
+            @RequestParam(name = "keyword") String keyword,
+            @RequestParam(name = "offset") long offset,
+            @RequestParam(name = "limit") int limit,
+            HttpServletRequest httpServletRequest
+    ) {
+        long memberId = Long.parseLong(httpServletRequest.getAttribute("memberId").toString());
+        return ResponseEntity.ok(new SpotifySearchTracksInfo());
     }
 }
