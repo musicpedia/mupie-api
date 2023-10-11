eeed347df729527aac96f9b8a75b9412ec8ec509
맹순영 <symaeng98@naver.com>
2023-10-11 15:34:06 +0900

    chore: member controller api 명세 작성

--- src/main/java/com/musicpedia/musicpediaapi/domain/member/controller/MemberController.java
+++ src/main/java/com/musicpedia/musicpediaapi/domain/member/controller/MemberController.java
@@ -2,10 +2,17 @@ package com.musicpedia.musicpediaapi.domain.member.controller;
 
 import com.musicpedia.musicpediaapi.domain.member.dto.MemberDetail;
 import com.musicpedia.musicpediaapi.domain.member.service.MemberService;
+import com.musicpedia.musicpediaapi.global.dto.AuthTokens;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +25,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     private final MemberService memberService;
 
+    @Operation(summary = "회원 정보 조회", description = "회원의 기본 정보, 평가 개수를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = MemberDetail.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST",
+                    content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE, schema = @Schema(type = "string"))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED",
+                    content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE, schema = @Schema(type = "string"))),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND",
+                    content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE, schema = @Schema(type = "string"))),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR",
+                    content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE, schema = @Schema(type = "string")))
+    })
     @GetMapping()
     public ResponseEntity<MemberDetail> getMember(HttpServletRequest httpServletRequest) {
         long memberId = Long.parseLong(httpServletRequest.getAttribute("memberId").toString());
-        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.getMemberDetail(memberId));
+        return ResponseEntity.ok(memberService.getMemberDetail(memberId));
     }
 }
