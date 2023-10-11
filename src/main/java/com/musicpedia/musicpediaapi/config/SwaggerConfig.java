7b19a3e33aa4e7102721e4ad24c6cd64d2b23ff7
맹순영 <symaeng98@naver.com>
2023-10-11 15:33:04 +0900

    chore: swagger 설정

--- src/main/java/com/musicpedia/musicpediaapi/config/SwaggerConfig.java
+++ src/main/java/com/musicpedia/musicpediaapi/config/SwaggerConfig.java
@@ -2,13 +2,26 @@ package com.musicpedia.musicpediaapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// SwaggerConfig.java
 @OpenAPIDefinition(
-        info = @Info(title = "musicpedia API",
-                version = "1.0"
-        )
-)
+        info = @Info(title = "Mupie API 명세서",
+                description = "음악 평점 앱 Mupie의 API 명세서입니다.",
+                version = "v1"))
+@RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
+        @Bean
+        public GroupedOpenApi mupieOpenApi() {
+                String[] paths = {"/v1/**"};
+
+                return GroupedOpenApi.builder()
+                        .group("Mupie API v1")
+                        .pathsToMatch(paths)
+                        .build();
+        }
 }
