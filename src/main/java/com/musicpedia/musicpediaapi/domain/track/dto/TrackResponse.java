aaa056530a1f6073205643f836f3b6807f47f30f
맹순영 <symaeng98@naver.com>
2023-11-04 11:19:33 +0900

    fix: 트랙 조회 시 평가 점수 및 조회 점수 제공하도록 변경

---
+++ src/main/java/com/musicpedia/musicpediaapi/domain/track/dto/TrackResponse.java
@@ -0,0 +1,16 @@
+package com.musicpedia.musicpediaapi.domain.track.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.musicpedia.musicpediaapi.domain.rating.dto.Score;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class TrackResponse {
+    private SpotifyTrack spotifyTrack;
+
+    private Score score;
+}
