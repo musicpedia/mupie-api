814125cb979e19be7a932d4c3c4331d59383be2d
맹순영 <symaeng98@naver.com>
2023-11-16 23:22:22 +0900

    feat: 이 주의 추천 곡 업데이트 자동화

--- src/main/java/com/musicpedia/musicpediaapi/domain/recommendation/week/service/WeeklyRecommendationService.java
+++ src/main/java/com/musicpedia/musicpediaapi/domain/recommendation/week/service/WeeklyRecommendationService.java
@@ -5,7 +5,9 @@ import com.musicpedia.musicpediaapi.domain.recommendation.week.dto.WeeklyRecomme
 import com.musicpedia.musicpediaapi.domain.recommendation.week.dto.WeeklyRecommendationResponse;
 import com.musicpedia.musicpediaapi.domain.recommendation.week.entity.WeeklyRecommendation;
 import com.musicpedia.musicpediaapi.domain.recommendation.week.repository.WeeklyRecommendationRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +18,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WeeklyRecommendationService {
     private final WeeklyRecommendationRepository weeklyRecommendationRepository;
+
+    private List<String> weeklyRecommendationTrackIds;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("이 주의 추천 곡 id를 가져옵니다.");
+        this.weeklyRecommendationTrackIds = findWeeklyRecommendationTrackIds();
+        System.out.println("id: " + weeklyRecommendationTrackIds.get(0));
+    }
+
+    @Scheduled(cron = "0 3 0 * * MON") // 매주 월요일 자정에 실행
+    public void updateWeeklyRecommendationTrackIds() {
+        this.weeklyRecommendationTrackIds = findWeeklyRecommendationTrackIds();
+    }
 
     public WeeklyRecommendationResponse getWeeklyRecommendation(Type type) {
         List<WeeklyRecommendationDetail> weeklyRecommendations = weeklyRecommendationRepository.findAllByType(type)
@@ -28,5 +44,16 @@ public class WeeklyRecommendationService {
                 .weeklyRecommendations(weeklyRecommendations)
                 .size(size)
                 .build();
+    }
+
+    private List<String> findWeeklyRecommendationTrackIds() {
+        return weeklyRecommendationRepository.findAllByType(Type.TRACK)
+                .stream()
+                .map(WeeklyRecommendation::getSpotifyId)
+                .toList();
+    }
+
+    public List<String> getWeeklyRecommendationTrackIds() {
+        return weeklyRecommendationTrackIds;
     }
 }
