15e90b3ccbd51d47d88c8c7c2453b6f26545dc35
맹순영 <symaeng98@naver.com>
2023-11-27 15:16:26 +0900

    refactor: 템플릿 메서드 활용하여 OAuthHelper 리팩토링

--- src/main/java/com/musicpedia/musicpediaapi/domain/auth/helper/OAuthHelper.java
+++ src/main/java/com/musicpedia/musicpediaapi/domain/auth/helper/OAuthHelper.java
@@ -2,9 +2,7 @@ package com.musicpedia.musicpediaapi.domain.auth.helper;
 
 import com.musicpedia.musicpediaapi.global.dto.OIDCDecodePayload;
 import com.musicpedia.musicpediaapi.global.dto.OIDCPublicKeysResponse;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public abstract class OAuthHelper {
     public OIDCDecodePayload getOIDCDecodePayload(String token) {
         OIDCPublicKeysResponse oidcOpenKeys = getOIDCOpenKeys();
