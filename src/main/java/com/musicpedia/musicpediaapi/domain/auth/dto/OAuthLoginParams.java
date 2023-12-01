e9899f9a3bb16252fded71f1cf6b41b9e38fbc1a
맹순영 <symaeng98@naver.com>
2023-12-01 21:41:32 +0900

    fix: auth validation 추가

--- src/main/java/com/musicpedia/musicpediaapi/domain/auth/dto/OAuthLoginParams.java
+++ src/main/java/com/musicpedia/musicpediaapi/domain/auth/dto/OAuthLoginParams.java
@@ -2,10 +2,12 @@ package com.musicpedia.musicpediaapi.domain.auth.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class OAuthLoginParams {
+    @NotBlank
     private String idToken;
 }
--- src/main/java/com/musicpedia/musicpediaapi/global/dto/AuthTokens.java
+++ src/main/java/com/musicpedia/musicpediaapi/global/dto/AuthTokens.java
@@ -1,13 +1,19 @@
 package com.musicpedia.musicpediaapi.global.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class AuthTokens {
+    @NotBlank
     private String accessToken;
+
+    @NotBlank
     private String refreshToken;
+
+    @NotBlank
     private String grantType;
 
     public static AuthTokens of(String accessToken, String refreshToken, String grantType) {
